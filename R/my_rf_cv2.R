2e35f2f97d3947339cf6bfa738b1742e6d102ffe
Amanda Kinney <amakinney@outlook.com>
2021-08-23 04:14:52 -0700

    Updating files after tutorials

--- R/my_rf_cv2.R
+++ R/my_rf_cv2.R
@@ -1,34 +1,39 @@
 #' Penguin body mass predictor
-#' 
+#'
 #' This function predicts the penguins body mass based on physical trait inputs.
-#' 
+#'
 #' @param k Numeric input representing the number of folds.
-#' 
+#'
 #' @return Numeric output representing the cv error.
-#' 
-#' @examples 
+#'
+#' @examples
 #'  my_rf_cv(5)
-#'  
+#'
 #' @export
 my_rf_cv <- function(k) {
-  
+
   folds <- sample(rep(1:k, length = nrow(data_cleaned)))
-  
-  for (i in 1:k) {
-    
+
+  for(i in 1:30) {
+    for (i in 1:k) {
+
     data_training <- data_cleaned %>% filter(folds != i)
     data_testing <- data_cleaned %>% filter(folds == i)
-    
-    modele <- randomForest(body_mass_g ~ bill_length_mm + bill_depth_mm + 
+
+    modele <- randomForest(body_mass_g ~ bill_length_mm + bill_depth_mm +
                              flipper_length_mm, data = data_training, ntree = 100)
-    
+
     predic <- predict(modele, data_testing[, -1])
-    
-    MSE <- (predic - data_cleaned$body_mass_g[i])^2
+
+    MSE <- data.frame((predic - data_cleaned$body_mass_g[i])^2)
+    }
+
+
   }
-  
+
+
   avg_MSE <- sum(MSE) / k
-  
+
   return(avg_MSE)
-  
+
 }
